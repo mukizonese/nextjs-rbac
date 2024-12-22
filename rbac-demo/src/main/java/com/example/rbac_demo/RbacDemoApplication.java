98bf70955627e875783277bdc4c11c8a89df0e73
mukesh.a.vast@tsgforce.com <mukesh.a.vast@tsgforce.com>
2024-12-22 09:10:33 +0530

    Initial Springboot - SPring Initialzr

---
+++ rbac-demo/src/main/java/com/example/rbac_demo/RbacDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbac_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacDemoApplication.class, args);
+	}
+
+}
